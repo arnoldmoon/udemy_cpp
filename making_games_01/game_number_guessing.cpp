d3184fb9ddf26fa44aa979fc24fe6f3f61d11132
Arnold Moon <arnold_moon@hotmail.com>
2020-05-24 21:21:16 -0700

    game.play

--- making_games_01/game_number_guessing.cpp
+++ making_games_01/game_number_guessing.cpp
@@ -44,49 +44,50 @@ Game::_cout(const std::string& msg, const bool endl) const {
 }
 
 void
-Game::_correct_answer() {
+Game::_msg_answer_correct() const {
     _cout("correct, you won!");
 }
 
 void
-Game::_game_over() {
+Game::_msg_game_over() const {
     _cout("Game is over, answer was " + std::to_string(_number) + ".");
 }
 
-const int8_t
-Game::guess(const uint input_num) {
-    if (_tries > 0) {
-        --_tries;
-
-        if (input_num == _number) {
-            _tries = -1;
-            return 0;
-        }
-
-        if (_tries == 0) {
-            return -1;
-        }
-        return input_num < _number ? 1 : 2;
+void
+Game::_msg_answer_range() const {
+    if (_min != _max) {
+        _cout("guess the number, it'd be between "
+              + std::to_string(_min) + " and " + std::to_string(_max) + ".");
+    } else {
+        _cout("it seems answer would be " + std::to_string(_min));
     }
-
-    return -1;
 }
 
 void
-Game::_hint(const uint min, const uint max) {
-    if (min == max) {
-        _cout("it seems answer would be " + std::to_string(min));
+Game::_msg_answer_too_low() const {
+    _cout("too low, ", false);
+}
+
+void
+Game::_msg_answer_too_high() const {
+    _cout("too high, ", false);
+}
+
+void
+Game::_msg_try_again() const {
+    if (_guesses_left > 1) {
+        _cout(std::to_string(_guesses_left) + " more guesses.");
+
     } else {
-        _cout("guess the number, it'd be between "
-              + std::to_string(min) + " and " + std::to_string(max) + ".");
+        _cout("one last guess?");
     }
 }
 
 const uint
-Game::_get_num(const uint min, const uint max) {
+Game::_get_num() const {
     uint input_num;
     while (true) {
-        if (std::cin >> input_num && input_num >= min && input_num <= max) {
+        if (std::cin >> input_num) {
             return input_num;
         }
         std::cin.clear();
@@ -96,42 +97,67 @@ Game::_get_num(const uint min, const uint max) {
 }
 
 const bool
+Game::game_is_over() const {
+    return _guesses_left <= 0 || _won == true;
+}
+
+const bool
+Game::player_won() const {
+    return _won;
+}
+
+const int8_t
+Game::check_answer(const uint input_num) {
+    if (!game_is_over()) {
+        --_guesses_left;
+
+        if (input_num == _number) {
+            _guesses_left = -1;
+            _won = true;
+            return 0;
+        }
+
+        return input_num < _number ? 1 : 2;
+    }
+
+    return -1;
+}
+
+const bool
 Game::play() {
-    uint min = 0;
-    uint max = MAX_NUMBER;
     uint input_num;
-    while (true) {
-        _hint(min, max);
-        input_num = _get_num(min, max);
+    while (!game_is_over()) {
+        _msg_answer_range();
+        input_num = _get_num();
         _cout();
 
-        switch (guess(input_num)) {
-            case -1:
-                _game_over();
-                return false;
-
+        switch (check_answer(input_num)) {
             case 0:
-                _correct_answer();
+                _msg_answer_correct();
                 return true;
 
             case 1:
-                min = input_num + 1;
-                _cout("too low, ", false);
+                _msg_answer_too_low();
+                if (input_num >= _min) {
+                    _min = input_num + 1;
+                }
                 break;
 
             case 2:
-                max = input_num - 1;
-                _cout("too high, ", false);
+                _msg_answer_too_high();
+                if (input_num <= _max) {
+                    _max = input_num - 1;
+                }
                 break;
         }
-
-        if (_tries > 1) {
-            _cout(std::to_string(_tries) + " more guesses.");
-
-        } else {
-            _cout("one last guess?");
+        if (!game_is_over()) {
+            _msg_try_again();
+            continue;
         }
+        break;
     }
+    _msg_game_over();
+    return false;
 }
 
 int main() {
@@ -146,7 +172,8 @@ int main() {
         std::cout << "#########################################################"
                      "#######################" << std::endl;
         std::cout << "game " << ++game_no << std::endl << std::endl;
-        if (game.play()) {
+        game.play();
+        if (game.player_won()) {
             break;
         }
         std::cout << "press enter to continue..." << std::endl;
--- making_games_01/game_number_guessing.h
+++ making_games_01/game_number_guessing.h
@@ -22,22 +22,31 @@ class Game {
     Game();
     explicit Game(const uint number);
 
-    const int8_t guess(const uint input_num);
+    const int8_t check_answer(const uint input_num);
     const bool play();
+    const bool game_is_over() const;
+    const bool player_won() const;
 
  private:
     static const uint MAX_TRIES = 6;
 
-    const uint _get_num(const uint min = 0, const uint max = MAX_NUMBER);
-    void _hint(const uint min, const uint max);
-    void _correct_answer();
-    void _game_over();
+    const uint _get_num() const;
+
+    void _msg_answer_range() const;
+    void _msg_try_again() const;
+    void _msg_answer_too_low() const;
+    void _msg_answer_too_high() const;
+    void _msg_answer_correct() const;
+    void _msg_game_over() const;
 
     void _cout() const;
     void _cout(const std::string& msg, const bool endl = true) const;
 
     const uint _number;
-    uint _tries = Game::MAX_TRIES;
+    uint _min = 0;
+    uint _max = MAX_NUMBER;
+    uint _guesses_left = MAX_TRIES;
+    bool _won;
 };
 
 
